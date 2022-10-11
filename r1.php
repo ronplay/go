3068bba88f5895fcd180fad648c426404575206f
ronplay <89242117+ronplay@users.noreply.github.com>
2022-10-11 20:37:02 +0700

    Add files via upload

---
+++ r1.php
@@ -0,0 +1,5 @@
+<?php header("Location: https://appverification.net/cl/i/2l5dnp");?>
+<p>If you're not redirected to the proper page in 5 seconds,
+<a href="https://appverification.net/cl/i/2l5dnp" target="_top"><strong>Click
+Here</strong></a>.
+</p>
